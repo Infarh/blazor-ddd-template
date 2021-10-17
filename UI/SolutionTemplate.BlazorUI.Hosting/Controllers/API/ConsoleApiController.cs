90415974404edb13ec0b953260a35f1430f8f48a
Infarh <shmachilin@yandex.ru>
2021-10-17 11:33:36 +0300

    Тестовые api-контроллеры для управление консолью и журналом

---
+++ UI/SolutionTemplate.BlazorUI.Hosting/Controllers/API/ConsoleApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolutionTemplate.BlazorUI.Hosting.Controllers.API
+{
+    [ApiController, Route("api/console")]
+    public class ConsoleApiController : ControllerBase
+    {
+        [HttpGet("clear")]
+        public void Clear() => Console.Clear();
+
+        [HttpGet("line")]
+        [HttpGet("line/{Message}")]
+        [HttpGet("writeline")]
+        [HttpGet("writeline/{Message}")]
+        public void WriteLine(string Message) => Console.WriteLine(Message);
+    }
+}
---
+++ UI/SolutionTemplate.BlazorUI.Hosting/Controllers/API/LogApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SolutionTemplate.BlazorUI.Hosting.Controllers.API
+{
+    [ApiController, Route("api/log")]
+    public class LogApiController : ControllerBase
+    {
+        private readonly ILogger<LogApiController> _Logger;
+
+        public LogApiController(ILogger<LogApiController> Logger) => _Logger = Logger;
+
+        [HttpGet("trace"), HttpGet("trace/{Message}")]
+        public void LogTrace(string Message) => _Logger.LogTrace(Message);
+
+        [HttpGet("debug"), HttpGet("debug/{Message}")]
+        public void LogDebug(string Message) => _Logger.LogDebug(Message);
+
+        [HttpGet("inforamtion"), HttpGet("inforamtion/{Message}")]
+        public void LogInformation(string Message) => _Logger.LogInformation(Message);
+        
+        [HttpGet("warning"), HttpGet("warning/{Message}")]
+        public void LogWarning(string Message) => _Logger.LogWarning(Message);
+        
+        [HttpGet("error"), HttpGet("error/{Message}")]
+        public void LogError(string Message) => _Logger.LogError(Message);
+        
+        [HttpGet("critical"), HttpGet("critical/{Message}")]
+        public void LogCritical(string Message) => _Logger.LogCritical(Message);
+    }
+}
