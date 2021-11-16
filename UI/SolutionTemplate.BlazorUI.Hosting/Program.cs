a32c34645c16524b5f659b2ed1854cd18d40f75a
Infarh <shmachilin@yandex.ru>
2021-11-16 16:30:50 +0300

    Переезд на .NET6

--- UI/SolutionTemplate.BlazorUI.Hosting/Program.cs
+++ UI/SolutionTemplate.BlazorUI.Hosting/Program.cs
@@ -1,31 +1,97 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using SolutionTemplate.DAL.Context;
+using SolutionTemplate.DAL.Repositories;
+using SolutionTemplate.DAL.Sqlite;
+using SolutionTemplate.DAL.SqlServer;
 
-namespace SolutionTemplate.BlazorUI.Hosting
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Logging.AddSerilog();
+
+//var configuration = builder.Configuration;
+var services = builder.Services;
+
+//var db_type = configuration["Database"];
+//switch (db_type)
+//{
+//    default: throw new InvalidOperationException($"База данных {db_type} не поддерживается");
+
+//    case "InMemory":
+//        services.AddDbContext<SolutionTemplateDB>(opt => opt.UseInMemoryDatabase("SolutionTemplateDb"))
+//           .AddTransient<IDbInitializer>(
+//                s => new SolutionTemplateDBInitializer(
+//                        s.GetRequiredService<SolutionTemplateDB>(),
+//                        s.GetRequiredService<ILogger<SolutionTemplateDBInitializer>>())
+//                    { Ignore = true })
+//           .AddSolutionTemplateRepositories();
+//        break;
+
+//    case "SqlServer":
+//        services.AddSolutionTemplateDbContextSqlServer(configuration.GetConnectionString(db_type));
+//        break;
+
+//    case "Sqlite":
+//        services.AddSolutionTemplateDbContextSqlite(configuration.GetConnectionString(db_type));
+//        break;
+//}
+
+//services.AddApiVersioning(opt =>
+//{
+//    opt.ReportApiVersions = true;
+//    opt.DefaultApiVersion = new ApiVersion(1, 0);
+//    opt.AssumeDefaultVersionWhenUnspecified = true;
+//    //opt.ApiVersionReader = new MediaTypeApiVersionReader("x-api-version");
+//    //opt.ApiVersionReader = new QueryStringApiVersionReader("x-api-version");
+//    opt.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+
+//});
+
+//services.AddSignalR();
+//services.AddMediatR(typeof(Program));
+//services.AddAutoMapper(typeof(Program));
+
+//services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "SolutionTemplate.WEB.API", Version = "v1" }));
+
+services.AddControllersWithViews();
+services.AddRazorPages();
+
+//services.AddResponseCompression();
+
+var app = builder.Build();
+//await using (var scope = app.Services.CreateAsyncScope())
+//{
+//    var initializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+//    await initializer.InitializeAsync();
+//}
+
+//app.UseResponseCompression();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            using var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var db_initializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-                await db_initializer.InitializeAsync();
-            }
-            await host.RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-               .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration))
-               .ConfigureWebHostDefaults(host => host
-                   .UseStartup<Startup>()
-                )
-        ;
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SolutionTemplate.WEB.API v1"));
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+//app.UseSerilogRequestLogging();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+await app.RunAsync();
--- UI/SolutionTemplate.BlazorUI/Program.cs
+++ UI/SolutionTemplate.BlazorUI/Program.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using SolutionTemplate.BlazorUI;
 
-namespace SolutionTemplate.BlazorUI
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var root_components = builder.RootComponents;
+root_components.Add<App>("#app");
+root_components.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var services = builder.Services;
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
+
